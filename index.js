3116938d4da307d28647614cf3105aba9fdda0ba
António Quaresma <antqua@outlook.pt>
2023-05-27 08:35:10 +0100

    Setting  dog cards modals

--- index.js
+++ index.js
@@ -7,3 +7,45 @@ function toggleDarkMode() {
 };
 
 darkModeToggleBtn.addEventListener('click', toggleDarkMode);
+
+
+// DOG CARDS MODALS -------------------------------------------------------------------
+
+const backdrop = document.getElementById('backdrop');
+
+function toggleModal(modalClass) {
+    const modal = document.querySelector(`.modal.card.${modalClass}`);
+
+    modal.classList.toggle('visible');
+    backdrop.classList.toggle('visible');
+}
+
+function hideModal(modalClass) {
+    const modal = document.querySelector(`.modal.card.${modalClass}`);
+
+    modal.classList.remove('visible');
+    backdrop.classList.remove('visible');
+}
+
+document.getElementById('chewie-button').addEventListener('click', function() {
+    toggleModal('chewie');
+});
+
+document.getElementById('chewie-cancel').addEventListener('click', function() {
+    toggleModal('chewie');
+});
+
+document.getElementById('spock-button').addEventListener('click', function() {
+    toggleModal('spock');
+});
+
+document.getElementById('spock-cancel').addEventListener('click', function() {
+    toggleModal('spock');
+});
+
+backdrop.addEventListener('click', function() {
+    hideModal('chewie');
+    hideModal('spock');
+});
+
+
